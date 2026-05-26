be17b4b68ae8fec39e839fdcd8703ce2aa312198
Akshun SHukla <akshunshukla123@gmail.com>
2026-05-26 12:55:36 +0530

    fix bug

--- server/src/app.js
+++ server/src/app.js
@@ -43,6 +43,17 @@ app.use("/api/v1/quote", quoteRouter);
 app.use("/api/v1/goal", verifyJWT, goalRouter);
 app.use("/api/v1/task", verifyJWT, taskRouter);
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Catch-all route to serve index.html for SPA routing (React Router)
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../../client/dist/index.html"));
+});
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
